a3f272cd328ac14ae616227b9953464c9fc192b8
Maxime DAPP <maxime.dapp@yahoo.fr>
2015-12-11 08:55:33 +0100

    [ADD] detTweetsPostedBy() to tweetTable.class.php

--- public_html/NeliSoft/model/tweetTable.class.php
+++ public_html/NeliSoft/model/tweetTable.class.php
@@ -9,11 +9,19 @@ class tweetTable
 
 	    $res = $connection->doQuery( $sql );
 
-	    return $res ;
+	    return $res;
 
 	}
 
-	public static function getTweetsPostedBy()
+	public static function getTweetsPostedBy($id_user){
+		$connection = new dbconnection() ;
+	    $sql = "select * from jabaianb.tweet where parent=".$id_user.";";
+
+	    $res = $connection->doQuery( $sql );
+
+	    return $res;
+
+	}
 }
 
 ?>
