425080a7db4ad66c7418975e123f1122def9c82b
Maxime DAPP <maxime.dapp@yahoo.fr>
2015-12-18 10:31:39 +0100

    fix errors

--- public_html/NeliSoft/model/postTable.class.php
+++ public_html/NeliSoft/model/postTable.class.php
@@ -14,7 +14,7 @@ class postTable
 
 	public static function addPost($user){
 		$connection = new dbconnection();
-		$sql = "INSERT INTO jabaianb.post (texte, date) VALUES ('".$_POST['tweet']."', ".date(Y-m-d H:i:s).");";
+		$sql = "INSERT INTO jabaianb.post (texte, date) VALUES ('".$_POST['tweet']."', '".date('Y-m-d H:i:s')."');";
 		$res = $connection->doQueryObject($sql, "post");
 		return $res;
 	}
